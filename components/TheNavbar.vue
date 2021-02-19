0c25555b2370c0d0c32739f6a587b6ed9fefa21b
phronesys <daniel.dazarola@gmail.com>
2021-02-19 16:25:28 -0300

    added more details in the sidebar, router links para nuevas 'páginas'

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -9,15 +9,16 @@
       </b-navbar-nav>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-lg-5">
-          <b-nav-item>Link 1</b-nav-item>
-          <b-nav-item>Link 2</b-nav-item>
-          <b-nav-item disabled>Disabled</b-nav-item>
+        <b-navbar-nav class="ml-lg-5" variant="success">
+          <b-nav-item to="/contacts">Contacts</b-nav-item>
+          <b-nav-item to="/add-contact">Add New Contact</b-nav-item>
+          <b-nav-item to="/calendar">Calendar</b-nav-item>
         </b-navbar-nav>
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown text="User" right>
-            <b-dropdown-item>Log in</b-dropdown-item>
-            <b-dropdown-item>Logout</b-dropdown-item>
+            <b-dropdown-item to="/user">User Details</b-dropdown-item>
+            <b-dropdown-item to="/login">Login</b-dropdown-item>
+            <b-dropdown-item to="/">Logout</b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -1,22 +1,69 @@
 <template>
   <div>
-    <b-button v-b-toggle.sidebar-1 variant="primary">Upcoming Events</b-button>
-    <b-sidebar id="sidebar-1" title="Next meeting" shadow bg-variant="dark" text-variant="light">
+    <b-button pill v-b-toggle.sidebar-1 variant="primary"
+      >Upcoming Events</b-button
+    >
+    <b-sidebar
+      id="sidebar-1"
+      title="Next meeting"
+      shadow
+      bg-variant="dark"
+      text-variant="light"
+    >
       <template #default="{ hide }">
         <div class="p-3">
-          <h4 id="sidebar-no-header-title">Custom header sidebar</h4>
+          <h4 id="sidebar-no-header-title">Kira</h4>
           <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+            Mi perro me observa mientras escribo código, esperando la ocación de
+            ir a pasear o de comer algo "rico" nuevo.
           </p>
-          <nav class="mb-3">
+          <nav class="mb-1">
             <b-nav vertical>
-              <b-nav-item active @click="hide">Active</b-nav-item>
-              <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
-              <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+              <b-nav-item active @click="hide">15:30 - 16:00</b-nav-item>
+              <b-nav-item>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-geo-alt-fill mr-2 mb-1"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"
+                  />
+                </svg>
+                <a href="https://www.google.cl/maps" target="_blank">
+                  The Direction, 69
+                </a>
+              </b-nav-item>
             </b-nav>
           </nav>
-          <b-button variant="primary" block @click="hide">Close Sidebar</b-button>
+          <b-button pill variant="outline-primary" class="mb-3 float-right"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-pencil-square"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+              /></svg
+          ></b-button>
+          <b-button
+            pill
+            to="/details"
+            variant="primary"
+            class="px-5"
+            @click="hide"
+            >Details</b-button
+          >
         </div>
       </template>
     </b-sidebar>
@@ -24,11 +71,8 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
