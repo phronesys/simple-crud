354e490f4e48e42f4cfd2d3d0a58b8f54ae08ee3
phronesys <daniel.dazarola@gmail.com>
2021-02-19 12:20:38 -0300

    first commit, added navbar and sidebar prototype

---
+++ components/TheNavbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark" sticky toggleable="lg">
+      <b-navbar-brand>My Contacts</b-navbar-brand>
+      <b-navbar-nav class="ml-lg-5">
+        <b-nav-item>
+          <the-sidebar></the-sidebar>
+        </b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-lg-5">
+          <b-nav-item>Link 1</b-nav-item>
+          <b-nav-item>Link 2</b-nav-item>
+          <b-nav-item disabled>Disabled</b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item>Log in</b-dropdown-item>
+            <b-dropdown-item>Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+import TheSidebar from "./TheSidebar.vue";
+export default {
+  components: { TheSidebar },
+};
+</script>
+
+<style>
+</style>
---
+++ components/TheSidebar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-button v-b-toggle.sidebar-1 variant="primary">Upcoming Events</b-button>
+    <b-sidebar id="sidebar-1" title="Next meeting" shadow bg-variant="dark" text-variant="light">
+      <template #default="{ hide }">
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Custom header sidebar</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </p>
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="hide">Active</b-nav-item>
+              <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+            </b-nav>
+          </nav>
+          <b-button variant="primary" block @click="hide">Close Sidebar</b-button>
+        </div>
+      </template>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
